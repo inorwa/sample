4840ad0bbff000746e2abb45e7c1fbdfcbdf5fb9
irek <inorwa@gmail.com>
2017-11-11 02:26:16 +0100

    initial version

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @version 0.1 start
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("START");
+        System.out.println("END");
+    }
+}
